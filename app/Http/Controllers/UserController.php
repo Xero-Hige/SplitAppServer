127eb2f5e3cbb1a6b5032c1350d96e5f84bb3ced
Agustín Santiago <agus.santiago@gmail.com>
2016-07-24 20:17:12 -0300

    Add documentation for user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,13 +10,23 @@ use App\Http\Requests;
 class UserController extends Controller
 {
     /**
+     * @apiVersion 0.0.1
+     *
      * @api {get} /token Request user token
+     * @apiDescription Returns the user token. If the user doesn't exist, it creates the user and returns data anyway.
+     * @apiName GetToken
+     * @apiGroup User
      *
      * @apiParam {String} facebook_id
      * @apiParam {String} facebook_token
      *
      * @apiSuccess {Int} user_id
      * @apiSuccess {String} token
+     * @apiSuccessExample {json} Success response
+     * {
+     *  "data": {"user_id": 1, "token": "sdsjakldasjdaldkj"},
+     *  "errors": []
+     * }
      */
     public function getToken(Request $request) {
         $user = User::whereFacebookId($request->input("facebook_id"))->first();
--- docs/api_project.js
+++ docs/api_project.js
@@ -6,7 +6,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-07-24T23:06:58.918Z",
+    "time": "2016-07-24T23:16:45.427Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
